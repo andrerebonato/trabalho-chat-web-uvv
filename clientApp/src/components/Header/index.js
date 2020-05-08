479e1852a660f3dc8e196d965f3764f8d181e0e1
André Rebonato <andrerebonato913@hotmail.com>
2020-05-08 12:40:55 -0300

    Initialized header component

--- clientApp/src/components/Header/index.js
+++ clientApp/src/components/Header/index.js
@@ -1,14 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './styles.css';
+import { CSSTransition } from "react-transition-group";
+import { NavbarToggler } from 'reactstrap';
 
-export const Header = ({ content, isCorno, hasColor }) => {
+const Header = ({ pageTitle, isLoggedIn }) => {
+    const [isHeaderVisible, setHeaderVisibility] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
 
+    //check window media query on instance this component
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 700px)");
+        mediaQuery.addListener(handleMediaQueryChange);
+        handleMediaQueryChange(mediaQuery);
 
+        return () => {
+            mediaQuery.removeListener(handleMediaQueryChange);
+        };
+    }, []);
+
+    //check if the window is on "smallscreen mode (<700px)"
+    const handleMediaQueryChange = mediaQuery => {
+        if (mediaQuery.matches) {
+            setIsSmallScreen(true);
+        } else {
+            setIsSmallScreen(false);
+        }
+    };
+
+    //open/close the header component.
+    const toggleHeader = () => {
+        setHeaderVisibility(!isHeaderVisible);
+    };
 
     return (
-        <h1 style={{ color: hasColor ? 'red' : 'blue' }}>
-            {
-                isCorno ? "É corno" : "Não eh"
-            }
-        </h1>
+        <header className="Header">
+            <h6 className="">Chat web uvv</h6>
+            <CSSTransition
+                in={!isSmallScreen || isHeaderVisible}
+                timeout={350}
+                classNames="NavAnimation"
+                unmountOnExit
+            >
+                <nav className="Nav">
+                    <a href="/">Home</a>
+                    <a href="/">Articles</a>
+                    <a href="/">About</a>
+                    {isLoggedIn ? (
+                        <button className="btn btn-danger">Logout</button>
+                    ) : (
+                            <button className="btn btn-primary">Entrar</button>
+                        )
+                    }
+                </nav>
+            </CSSTransition>
+            <button onClick={toggleHeader} className="Burger">
+                Menu
+            </button>
+        </header>
     )
 }
+
+export default Header;
